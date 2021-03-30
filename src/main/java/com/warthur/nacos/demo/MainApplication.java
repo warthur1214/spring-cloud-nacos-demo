323027efa6784d58327eba7cd3f7b14e095a774b
warthur <warthur@163.com>
2021-03-30 17:17:37 +0800

    引入sharding-jdbc分布式事务

--- src/main/java/com/warthur/nacos/demo/MainApplication.java
+++ src/main/java/com/warthur/nacos/demo/MainApplication.java
@@ -2,6 +2,7 @@ package com.warthur.nacos.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
  * @date 2020/11/27
  */
 @SpringCloudApplication
+@ComponentScan("io.shardingsphere.transaction.aspect")
 public class MainApplication {
 
     public static void main(String[] args) {
